a2c2ce88b4d6028d244420261c7691374ee9c240
ovi bojor <ovidiu.bojor@happeo.com>
2020-12-21 14:13:46 +0200

    custom widget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    function listener(event) {
+      const { data } = event;
+
+      if (!data._h || typeof data._h !== "object") {
+        // This is not coming from Happeo
+        return;
+      }
+
+      if (data._h.userinfo) {
+        // Case, Happeo sent us userinfo, we should do stuff with it
+        const { primaryEmail } = data._h.userinfo;
+        console.log(primaryEmail);
+      }
+    }
+    console.log('asdasd')
+    window.addEventListener("message", listener);
+
+    window.parent.postMessage(
+      { _c: { id: "test", scopes: ["userinfo.email"] } },
+      "*"
+    );
+
+    return () => {
+      window.removeEventListener("message", listener);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
